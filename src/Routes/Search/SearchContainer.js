e94d09ed5d31278ee19273360b421b8b98d95e8b
xiah <xiah0526@gmail.com>
2020-03-22 09:33:02 +0900

    Search Container update

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,14 +1,45 @@
 import React, {Component} from 'react';
 import SearchPresenter from "./SearchPresenter";
-
+import { moviesApi, tvApi} from "../../api";
 
 class SearchContainer extends Component {
     state = {
         movieResults: null,
         tvResults: null,
-        searchTerm: null,
+        searchTerm: "",
         loading: false,
-        error: null
+        error: null,
+
+    };
+
+    handleSubmit = () => {
+        const {searchTerm} = this.state;
+        if (searchTerm !== "") {
+            this.SearchByTerm();
+        }
+    };
+
+    SearchByTerm = async() => {
+        const {searchTerm} = this.state;
+        this.setState({loading: true});
+        try {
+            const {
+                data: { result: movieResults }
+            } = await moviesApi.search(searchTerm);
+
+            const {
+                data: { result: tvResults }
+            } = await tvApi.search(searchTerm);
+
+            this.setState({
+                movieResults,
+                tvResults
+            })
+        } catch {
+            this.setState({error: "Can't find result!"});
+        } finally {
+            this.setState({loading: false});
+        }
     };
 
     render() {
@@ -27,6 +58,7 @@ class SearchContainer extends Component {
                 searchTerm={searchTerm}
                 loading={loading}
                 error={error}
+                handleSubmit={this.handleSubmit}
            />
         );
     }
