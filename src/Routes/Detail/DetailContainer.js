cef5af36b2eacf57f21f413e80bba67c95b15277
xiah <xiah0526@gmail.com>
2020-03-22 11:13:38 +0900

    Details update

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,12 +1,50 @@
 import React, {Component} from 'react';
 import DetailPresenter from "./DetailPresenter";
+import {tvApi, moviesApi} from '../../api';
 
 class DetailContainer extends Component {
-   state = {
-       result : null,
-        error: null,
-        loading: true
-    };
+    constructor(props) {
+        super(props);
+        const {location: {pathname}} = props;
+
+        this.state = {
+            result: null,
+            error: null,
+            loading: true,
+            isMovie: pathname.includes("/movie/")
+        };
+    }
+
+
+    async componentDidMount() {
+        const {
+            match : {
+                params: {id}
+            },
+            history: { push }
+        } = this.props;
+
+        const { isMovie } = this.state;
+        const parsedId = parseInt(id);
+        if (isNaN(parsedId)) {
+            return push("/");
+        }
+        let result = null;
+        try {
+            if (isMovie) {
+                ({data: result} = await moviesApi.movieDetail(parsedId));
+            } else {
+                ({data: result} = await tvApi.showDetail(parsedId));
+            }
+            this.setState({
+                result
+            })
+        } catch {
+            this.this.setState({error: "Can't find anything!"})
+        } finally {
+            this.setState({loading: false })
+        }
+    }
 
     render() {
         const {result, error, loading} = this.state;
--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,10 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from "styled-components";
 import Loader from '../../Components/loader';
+import Message from "../../Components/Message";
 
 const Container = styled.div`
-  height: clac(100vh-50px);
+  height: calc(100vh - 50px);
   width: 100%;
   position: relative;
   padding: 50px;
@@ -17,7 +18,7 @@ const Backdrop = styled.div`
   width: 100%;
   height: 100%;
   background-image: url(${props => props.bgImage});
-  background-position: center;
+  background-position: center center;
   background-size: cover;
   filter: blur(3px);
   opacity: 0.5;
@@ -25,7 +26,7 @@ const Backdrop = styled.div`
 `;
 
 const Content = styled.div`
-  display: flex;
+  display: flex;s
   width: 100%;
   position:relative;
   z-index: 1;
@@ -40,6 +41,32 @@ const Cover = styled.div`
   border-radius: 5px;
 `;
 
+const Data = styled.div`
+  width: 70%;
+  margin-left: 10px;
+`;
+
+const Title = styled.h3`
+  font-size: 32px;
+`;
+
+const ItemContainer = styled.div`
+  margin: 20px 0;
+`;
+const Divider = styled.span`
+  margin: 0 10px;
+`;
+
+
+const Item = styled.div``;
+
+const OverView = styled.p`
+  font-size: 13px;
+  opacity: 0.7;
+  line-height: 1.7;
+  width: 80%;
+`;
+
 const DetailPresenter = ({
     result,
     loading,
@@ -49,17 +76,53 @@ const DetailPresenter = ({
         <Loader /> : (
             <Container>
                 <Backdrop
-                    bgImage={`https://image.tmdb.org/t/p/orignal${result.backdrop_path}`}
+                    bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}
                 />
                 <Content>
                     <Cover
                         bgImage={
                             result.poster_path ?
-                                `https://image.tmdb.org/t/p/orignal${result.poster_path}`
+                                `https://image.tmdb.org/t/p/original${result.poster_path}`
                                 : require("../../image/empty-image.png")
                         }
                     />
+                    <Data>
+                        <Title>
+                            {
+                                result.original_title ? result.original_title : result.original_name
+                            }
+                        </Title>
+                        <ItemContainer>
+                            <Item>
+                                {
+                                    result.release_date
+                                    ? result.release_date.substring(0,4)
+                                        : result.first_air_date.substring(0,4)
+                                }
+                            </Item>
+                            <Divider>•</Divider>
+                            <Item>
+                                {
+                                    result.runtime ? result.runtime : result.episode_run_time[0]
+                                } min
+                            </Item>
+                            <Divider>•</Divider>
+                            <Item>
+                                {
+                                    result.genres &&
+                                        result.genres.map((genre, index) =>
+                                            index === result.genres.length - 1
+                                            ? genre.name
+                                                : `${genre.name} / `
+                                        )
+                                }
+                            </Item>
+                            <Divider>•</Divider>
+                        </ItemContainer>
+                        <OverView>{result.overview}</OverView>
+                    </Data>
                 </Content>
+                {error && <Message color="#e74c3c" text={error}/>}
             </Container>
         )
 );
